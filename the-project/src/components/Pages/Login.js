062b6535a4a18695262e49d8bf85ff9901f6cc50
DannyDoneva96 <danielasinmas1@gmail.com>
2022-11-23 13:47:15 +0200

    stylizing login page

--- the-project/src/components/Pages/Login.js
+++ the-project/src/components/Pages/Login.js
@@ -1,5 +1,27 @@
+import loginImg from '../../images/login-img.jpg'
+import Logo from '../../images/logo.png'
 
-export  const Login = (props) =>{
+export const Login = (props) => {
     return (
-       <h1>Login page</h1>)
+
+        <div className="main-login-container">
+            <img src={loginImg} alt="img" />
+            <div className="container">
+                <div className="logo-login">  <img src={Logo} alt="img" />
+                </div>
+                <div className="main-div-login">
+                    <form className="form-login">
+                        <label for="username" name="username" >Потребител</label>
+                        <input type="text" name="username" ></input>
+                        <label for="password" name="password" >Парола</label>
+                        <input type="text" name="password" ></input>
+
+                        <button className="btn-login"><i class="fa-regular fa-paper-plane"></i></button>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+
+    )
 }
