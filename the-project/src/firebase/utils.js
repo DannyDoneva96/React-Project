59cf2b0b8ef32a71aa67e958ba4cbf006e30004c
DannyDoneva96 <danielasinmas1@gmail.com>
2022-11-23 14:59:32 +0200

    try to hide config files

---
+++ the-project/src/firebase/utils.js
@@ -0,0 +1,16 @@
+
+import firebaseConfig from './config'
+import {getFirestore} from '@firebase/firestore';
+import {getAuth} from 'firebase/auth'
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db=getFirestore(app)
+export const auth = getAuth(app)
