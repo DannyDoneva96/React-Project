abb401dfe0a73f7cd1cd9282594b96f1c77eff42
Martin B <m.bogdanov9110@gmail.com>
2022-11-26 15:34:09 +0200

    commented out Home, not needed for now!

--- the-project/src/components/Pages/Home.js
+++ the-project/src/components/Pages/Home.js
@@ -1,14 +1,12 @@
-import { Link } from 'react-router-dom';
-import { Navigation } from "../common/Navigation";
-import { Footer } from "../common/Footer";
-import Logo from '../../images/logo.png';
+// import { Link } from 'react-router-dom';
+// import { Navigation } from "../common/Navigation";
+// import { Footer } from "../common/Footer";
+// import Logo from '../../images/logo.png';
 
-export const Home = (props) => {
-    return (
-        <div>
-            <Navigation />
+// export const Home = (props) => {
+//     return (
+//         <div>
 
-            <Footer />
-        </div>)
+//         </div>)
 
-}
+// }
