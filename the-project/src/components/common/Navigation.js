49bdc89374b0fb5f7906294854f4f3335ba6f81d
DannyDoneva96 <danielasinmas1@gmail.com>
2022-11-28 11:21:47 +0200

    update
    Changed the logo picture
    remove fixed position (it goes if front of the  main content )
    add some styling

--- the-project/src/components/common/Navigation.js
+++ the-project/src/components/common/Navigation.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom'
-import Logo from '../../images/logo.png'
+import Logo from '../../images/new-logo.png'
 
 export const Navigation = () => {
 
